86b30436f9df93d40bf9ca0fc98276bab3c40123
Chandan Gowda B S <g-chandan@inahosolutions.com>
2024-06-17 10:45:30 +0530

    Adding event items

--- model/eventModel.js
+++ model/eventModel.js
@@ -3,6 +3,7 @@ const mongoose = require("mongoose");
 const eventSchema = new mongoose.Schema({
   eventName: String,
   eventTheme: String,
+  eventItem:[String],
   eventPrice: String,
   eventImageUrls: [String],
 });
